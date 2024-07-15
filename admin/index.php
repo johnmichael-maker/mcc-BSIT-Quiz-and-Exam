00dfe0066e8613bcbfb391a3b0eb7db4abf244fb
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-07-15 11:51:25 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -45,7 +45,7 @@
                <div class="col-lg-12">
                         <div class="card">
                             <div class="card-body">
-                                <canvas id="myChart" width="400" height="90"></canvas>
+                                <canvas id="myChart" width="400" height="95"></canvas>
                             </div>
                         </div>
                     </div>
