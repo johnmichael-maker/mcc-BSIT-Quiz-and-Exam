2a701a86d08f9b7a01c6e30198e861e5e7943058
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-07-15 11:31:08 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -42,12 +42,55 @@
                             </div>
                         </div>
                     </div>
+               <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <canvas id="myChart" width="400" height="100"></canvas>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-
         </div>
-
     </div>
 </div>
+<script>
+   const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Quizzes', 'Exams', 'Quiz Student', 'Examinees'],
+        datasets: [{
+            label: '# of Items',
+            data: [
+                <?= $adminController->getAllQuestionCount() ?>, 
+                <?= $adminController->examCount() ?>, 
+                <?= $adminController->contestantsCount() ?>, 
+                <?= $adminController->examineesCount() ?>
+            ],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.8)',  // Darker red
+                'rgba(54, 162, 235, 0.8)',  // Darker blue
+                'rgba(255, 206, 86, 0.8)',  // Darker yellow
+                'rgba(64, 224, 208, 0.8)'   // Darker red
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+</script>
+</script>
 <?php require __DIR__ . '/./partials/footer.php' ?>
