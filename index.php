a4aa718e98385bba6bc73fcd2ac158a7a8f8e081
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-07-17 11:03:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -58,6 +58,9 @@
                     <li class="nav-item">
                         <a href="about.php" class="nav-link">About</a>
                     </li>
+                      <li class="nav-item">
+                        <a href="" class="nav-link">Contact As</a>
+                    </li>
                     <li class="nav-item">
                         <a href="signup.php" class="nav-link">Login</a>
                     </li>
