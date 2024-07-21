84dafbf991431cee06b9f812334903b0e11eff04
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-07-21 20:40:35 +0800

    Update add-exam.php

--- admin/add-exam.php
+++ admin/add-exam.php
@@ -49,7 +49,7 @@
                         <option value="2">East</option>
                         <option value="3">West</option>
                         <option value="4">South</option>
-                        <option value="5">Suth East</option>
+                        <option value="5">South East</option>
                     </select>
 
                     <label for="">Year Level</label>
