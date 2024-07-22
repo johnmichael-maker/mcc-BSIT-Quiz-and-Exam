56ab1e187a228f4b4a2d28ee85221c257aeb813d
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-07-22 11:31:32 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -23,7 +23,7 @@ require __DIR__ . '/./partials/header.php';
     }
 </style>
 
-<body class="py-5" >
+<body >
     <div class="h-100-vh d-lg-flex align-items-lg-center justify-content-lg-center position-relative" style="max-height: 100vh;">
    
         <div class="container">
