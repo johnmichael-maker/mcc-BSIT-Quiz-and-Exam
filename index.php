484fa9cccb0becf242a36e58986a45dfd39745c9
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-07-29 14:37:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
           @keyframes fadeInRight {
     from {
         opacity: 0;
-        transform: translateX(50px);
+        transform: translateX(80px);
     }
     to {
         opacity: 1;
@@ -49,7 +49,7 @@
 @keyframes fadeInLeft {
     from {
         opacity: 0;
-        transform: translateX(-50px);
+        transform: translateX(-80px);
     }
     to {
         opacity: 1;
