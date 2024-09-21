da7331d088eb479522d7bf92494185a7d6c577d0
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-09-21 19:43:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -456,7 +456,7 @@ h6.fw-bold {
                 <div class="col-lg-8 h-100 my-auto text-light">
                     <h1 class="fade-in-right">MADRIDEJOS COMMUNITY COLLEGE</h1>
                     <h3 class="fade-in-left">Pop Quiz and Exam</h3>
-                    <p>Copyright © 2024 Madridejos Community College | Create by John Michaelle Robles</p>
+                       <p>&copy; John Michaelle Robles 2024</p>
                 </div>
             </div>
         </div>
@@ -564,7 +564,7 @@ h6.fw-bold {
                 <i class="fas fa-phone" style="font-size: 20px;"></i>
             </a>
         </div>
-            <p class="mt-3">&copy; John Michaelle Robles 2024</p>
+           <p class="mt-3">Copyright © 2024 Madridejos Community College | Create by John Michaelle Robles</p>
         </div>
     </footer>
     
