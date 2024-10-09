8d6d989490e08e5bfcb8016c96cb30b580a24b54
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-10-09 04:08:38 -0700

    commit

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
     $feedbacks = $databaseController->getFeedbacks();
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MCC Competition : QUIZ BOWL</title>
