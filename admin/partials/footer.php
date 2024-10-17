e85ee816a27173d61a64901d2be4b56844542b2d
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-10-17 13:07:14 +0800

    commit

---
+++ admin/partials/footer.php
@@ -0,0 +1,46 @@
+<!-- <div class="alert-modal d-none" id="alert-modal">
+    <div class="card position-relative bg-transparent pt-4 border-0 success-card d-none">
+        <div class="position-absolute top-0 text-center w-100 success-icon">
+            <img src="../assets/img/check-circle-svgrepo-com.png" alt="Icon-png">
+        </div>
+        <div class="card-body bg-light text-center pt-4 text-success">
+            <h1>Logged Out Successfully</h1>
+            <p>Your account has been logged out successfully.</p>
+        </div>
+    </div>
+</div> -->
+<?php
+  if (isset($_GET['logout'])) {
+        
+        ?>
+
+    <script>
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Logged out successfully",
+            showConfirmButton: false,
+            timer: 1500
+            }).then(() => {
+                window.location.href = "../index.php"
+            });
+    </script>
+<?php
+session_destroy();
+    }
+    ?>
+
+
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/dataTable.js"></script>
+<script src="../assets/js/bootstrap.js"></script>
+<script src="../assets/js/admin.js"></script>
+
+<script>
+    $(document).ready(function(){
+        $("#dataTable").DataTable();
+    })
+</script>
+</body>
+
+</html>
