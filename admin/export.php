6b9e0d3cede40763939ba1a56771a08ef3b6793a
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-10-17 13:20:53 +0800

    Update export.php

--- admin/export.php
+++ admin/export.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/csv');
 header('Content-Disposition: attachment; filename="users_data.csv"');
 
-$conn = new mysqli("localhost", "root", "", "mcc_bsit_quiz_and_exam");
+$conn = new mysqli("localhost", "u510162695_bsit_quiz", "1Bsit_quiz", "u510162695_bsit_quiz");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
