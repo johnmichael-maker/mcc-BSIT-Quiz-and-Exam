8c623e728a0feeb4b82e165bbe998bfe0868a047
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-10-30 16:56:15 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -13,6 +13,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="shortcut icon" href="assets/img/bsit-logo.png" type="">
     <style>
     
 body {
