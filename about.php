3adbd5782d2d8106f729a65981eb93ae0234d85a
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-10-30 16:46:49 +0800

    Update about.php

--- about.php
+++ about.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
     
-dy {
+body {
     color: hsl(0, 0%, 20%); 
 }
 
