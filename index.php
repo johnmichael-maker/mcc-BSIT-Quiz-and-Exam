feb65b0312a2e186d5ae0f82c4404d69b11fc472
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-10-30 16:17:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ body {
 }
 
 header {
-    background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.4)), url(./assets/img/mcc-bg.jpg);
+     background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4)), url(./assets/img/mcc-bg.jpg);
     background-size: cover;
     background-position: center;
     padding: 0;
