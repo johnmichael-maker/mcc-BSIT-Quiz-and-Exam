c9c901501ebce735f20d36eb03192e1727f679f2
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-11-05 00:47:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -609,7 +609,7 @@ h6.fw-bold {
         <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
         style="background-color: #f8f9fa; border-color: #c82333; padding: 8px; border-radius: 4px;">
     <span class="navbar-toggler-icon"></span>
-</button
+</button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
