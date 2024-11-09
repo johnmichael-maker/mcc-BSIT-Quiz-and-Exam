a8edc6d43fc9a00bea0804a96cbb54ded1b27807
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-11-09 10:48:14 +0800

    Update instructors.php

--- admin/instructors.php
+++ admin/instructors.php
@@ -72,7 +72,7 @@ try {
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h5><i class="bx bx-user"></i> Instructor (User)</h5>
+                                <h5><i class="bx bx-user"></i> Instructor</h5>
                             </div>
 
                             <div class="card-body">
