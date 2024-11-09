12b053e4be88d1f9e47187895b3af4b187e6154b
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-11-09 11:35:09 +0800

    Update step_instructor.php

--- step_instructor.php
+++ step_instructor.php
@@ -197,7 +197,6 @@ input[type="submit"]:hover {
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
- 
     <script>
     function validateEmail(event) {
         event.preventDefault(); 
@@ -258,6 +257,6 @@ input[type="submit"]:hover {
             confirmButtonText: 'OK'
         });
     }
-    </script>>
+    </script>
 </body>
 </html>
