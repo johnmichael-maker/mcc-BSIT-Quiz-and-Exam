eadb8f0f08acca291244442f0469e468182a0151
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-11-17 14:00:41 +0800

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -67,6 +67,7 @@ Secret
                                 </span>
                             </div>
                             <input type="file" id="fileInput" name="image" accept="image/*" style="display: none;">
+                             <div class="g-recaptcha" data-sitekey="6Ld-fYEqAAAAAHbSvaJjesYOnT7kXZWRmQE4njI-"></div>
                             <button type="submit" name="button" class="btn w-100 btn-danger mt-3 mb-2">Login</button>
                             <br>
                             <p style="float: left; margin-top: 10px;">
