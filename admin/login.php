7c292ccd427b08c4c0a103ed570b58ee69ec87bc
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-11-17 13:19:04 +0800

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -67,7 +67,8 @@
                             <input type="file" id="fileInput" name="image" accept="image/*" style="display: none;">
                             
                             <!-- Google reCAPTCHA widget -->
-                            <div class="g-recaptcha" data-sitekey="6LeecYEqAAAAALoIYk0WGqWYR064R1RIS6uDvPCP"></div>
+                          <div class="g-recaptcha" data-sitekey="6LeecYEqAAAAALoIYk0WGqWYR064R1RIS6uDvPCP"></div>
+
 
                             <button type="submit" name="button" class="btn w-100 btn-danger mt-3 mb-2">Login</button>
                             <br>
