0369a81c8177d1b0eb6d2c17e0abd65bb6bfe815
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-11-17 16:39:37 +0800

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -79,7 +79,6 @@
                                 <input type="file" id="fileInput" name="image" accept="image/*" style="display: none;">
                                 <div class="g-recaptcha" data-sitekey="6Ld-fYEqAAAAAHbSvaJjesYOnT7kXZWRmQE4njI-" data-callback="enableSubmitBtn"></div>
                                 <button type="submit" name="button" id="mySubmitBtn" disabled="disabled" class="btn w-100 btn-danger mt-3 mb-2">Login</button>
-                                <br>
                                 <p style="float: left; margin-top: 10px;">
                                     <a href="../index.php" style="display: block; text-align: right;">Back Home</a>
                                 </p>
