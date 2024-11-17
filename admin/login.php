28e67bb001373d53a20da5627784cff327ac8819
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-11-17 15:36:28 +0800

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -36,7 +36,7 @@
             }
     /* Make the reCAPTCHA widget responsive */
     .g-recaptcha {
-        transform: scale(0.9);
+        transform: scale(0.7);
         transform-origin: 0 0; 
         width: 100% !important;  
         max-width: 400px; 
