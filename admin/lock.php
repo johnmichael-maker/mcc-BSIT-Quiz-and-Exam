c51eea2b54425307a6631df3c9ee787627bb8856
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-11-20 02:44:35 +0800

    commit

--- admin/lock.php
+++ admin/lock.php
@@ -1,17 +1,11 @@
 <?php
+// Database connection
+$conn = new mysqli("localhost", "u510162695_bsit_quiz", "1Bsit_quiz", "u510162695_bsit_quiz");
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mcc_bsit_quiz_and_exam";
-
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} // Database connection file
+}
+
 
 // Check if email is provided
 if (isset($_POST['email'])) {
