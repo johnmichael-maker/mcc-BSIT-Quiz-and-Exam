38616d585dcca12135efb43071a309cdb0a079ca
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-11-20 13:59:44 +0800

    Update access-control.php

--- admin/access-control.php
+++ admin/access-control.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Admin | Access of mccbistquiandexam</title>
+      <link rel="icon" href="../assets/img/file.png">
 
     <!-- Include Google Fonts and Font Awesome (for spinner) -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
