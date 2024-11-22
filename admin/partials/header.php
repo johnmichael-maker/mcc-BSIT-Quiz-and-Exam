c0893210fea9b145537440dad2d1885c61bf4054
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-11-22 14:45:42 +0800

    Update header.php

--- admin/partials/header.php
+++ admin/partials/header.php
@@ -7,7 +7,7 @@
     $adminController = new Admin($_POST);
     // $adminController->startSession();
     if ($adminController->checkAdmin()) {
-        header('location: access-control.php');
+        header('location: login.php');
     }
     if ($adminController->isAdminDashboard()) {
         if (!$adminController->isActive()) {
