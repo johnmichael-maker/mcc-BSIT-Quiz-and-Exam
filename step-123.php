3312dc82ec1d606845feb799202b62c8c616dfb5
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-11-26 16:59:29 +0800

    Update step-123.php

--- step-123.php
+++ step-123.php
@@ -357,7 +357,7 @@ $username = $user['Username'];  // Get username from the database
         <div class="page">
             <div class="title">Contact Info:</div>
            <div class="field">
-    <div class="label">Username</div>
+    <div class="label">MS 365 Username</div>
     <input type="text" name="Username" id="username" placeholder="MS 365 Email"
            value="<?php echo htmlspecialchars($username); ?>" 
            readonly required />
