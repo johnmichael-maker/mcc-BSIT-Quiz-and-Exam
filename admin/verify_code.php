edde19d09e4dfeba2a65e7a4adbb4caaf4246d21
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-11-30 01:45:19 +0800

    Update verify_code.php

--- admin/verify_code.php
+++ admin/verify_code.php
@@ -30,7 +30,7 @@ if (isset($_POST['code']) && isset($_POST['email'])) {
               $_SESSION['email_verified'] = true;
             
             // Return success response and perform redirect
-            echo json_encode(['success' => true, 'message' => 'Verification successful. Redirecting...', 'redirect' => 'login.php']);
+            echo json_encode(['success' => true, 'message' => 'Verification successful. Redirecting...', 'redirect' => 'login']);
         } else {
             echo json_encode(['success' => false, 'message' => 'Invalid verification code.']);
         }
