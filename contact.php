aa05b53592f0e52efac2fe09130089e48a66cea2
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-12-08 17:15:53 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,9 +1,9 @@
 <?php
 // Database credentials
 $servername = "localhost"; // your MySQL server
-$username = "u510162695_itinventory"; // your MySQL username
-$password = "1Itinventory_pass"; // your MySQL password
-$dbname = "u510162695_itinventory"; // your database name
+$username = "u510162695_rio-lawis"; // your MySQL username
+$password = "1Rio-lawis_pass"; // your MySQL password
+$dbname = "u510162695_rio-lawis"; // your database name
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password);
