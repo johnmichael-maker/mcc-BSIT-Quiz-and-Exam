bb02b8291a9ba185349c71b175f66621bc4b3dc9
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-12-20 03:12:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
      $databaseController = new DatabaseControl;
     $feedbacks = $databaseController->getFeedbacks();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
 <!DOCTYPE html>
 <html lang="en">
