becc179c1a59da9875e01093c9e019603de6a9c1
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-12-19 14:40:02 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT COUNT(id) AS user_count FROM ms_365_users";
+$sql = "SELECT COUNT(id) AS user_count FROM ms_365_instructor";
 $result = $conn->query($sql);
 if ($result) {
     $row = $result->fetch_assoc();
