74bd6cdc504436a6b0ecf69ed630620263f188b4
johnmichael-maker <johnmichaelrobles813@gmail.com>
2024-12-22 04:25:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php 
      require __DIR__ . '/./vendor/autoload.php';
-     use App\DatabaseControl;
-
-     $databaseController = new DatabaseControl;
-    $feedbacks = $databaseController->getFeedbacks();
-
+     
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
