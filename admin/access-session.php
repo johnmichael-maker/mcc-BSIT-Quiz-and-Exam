39c51f39a358094c1ab0605c61bbd48b3505be2e
johnmichael-maker <johnmichaelrobles813@gmail.com>
2025-02-22 11:42:13 +0800

    Update access-session.php

--- admin/access-session.php
+++ admin/access-session.php
@@ -220,7 +220,7 @@ input[type="email"].form-control:focus {
                 <form id="email-form">
                     <div class="input-group" style="flex-direction: column; align-items: center;">
                         <input type="email" class="form-control" name="email" placeholder="Send Email for Verification " required>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <!--<button type="submit" class="btn btn-primary">Submit</button>-->
 
                     </div>
                 </form>
