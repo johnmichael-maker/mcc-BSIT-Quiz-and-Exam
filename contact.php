7efe6b542c2ae3d86d506df93e615825f209011a
johnmichael-maker <johnmichaelrobles813@gmail.com>
2025-03-20 01:12:44 -0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -8,21 +8,24 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "CREATE TABLE IF NOT EXISTS `admin` (
-        `admin_id` INT(11) NOT NULL AUTO_INCREMENT,
-        `username` VARCHAR(50) DEFAULT NULL,
-        `img` TEXT DEFAULT NULL,
-        `email` VARCHAR(255) NOT NULL,
-        `password` VARCHAR(255) DEFAULT NULL,
-        `verification` VARCHAR(255) DEFAULT NULL,
-        `userType` INT(1) NOT NULL DEFAULT 1,
-        `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    $sql = "CREATE TABLE IF NOT EXISTS `examinees` (
+        `id` INT(11) NOT NULL AUTO_INCREMENT,
+        `id_number` VARCHAR(255) NOT NULL,
+        `section` INT(11) NOT NULL,
+        `year_level` INT(11) NOT NULL,
+        `fname` TEXT NOT NULL,
+        `lname` TEXT NOT NULL,
+        `mname` TEXT DEFAULT NULL,
+        `exam_id` INT(11) DEFAULT NULL,
+        `score` INT(11) DEFAULT NULL,
+        `status` INT(11) NOT NULL DEFAULT 1,
+        `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        PRIMARY KEY (`admin_id`)
+        PRIMARY KEY (`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
 
     $conn->exec($sql);
-    echo "Table `admin` created successfully!";
+    echo "Table `examinees` created successfully!";
 } catch(PDOException $e) {
     echo "Error creating table: " . $e->getMessage();
 }
