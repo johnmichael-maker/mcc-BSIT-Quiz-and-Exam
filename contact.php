ddfb1e9da0a9afcb7d246f4c9cb56cf59acf6f0b
johnmichael-maker <johnmichaelrobles813@gmail.com>
2025-03-25 23:44:18 -0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,18 +9,33 @@ try {
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 
-    $sql = "CREATE TABLE IF NOT EXISTS login_history (
+    $sql = "CREATE TABLE IF NOT EXISTS ms_365_instructor (
         id INT(11) NOT NULL AUTO_INCREMENT,
-        ip_address VARCHAR(45) NOT NULL,
-        username VARCHAR(255) NOT NULL,
-        status ENUM('success','failure') NOT NULL,
-        reason VARCHAR(255) DEFAULT NULL,
-        attempt_time DATETIME DEFAULT current_timestamp(),
+        first_name VARCHAR(100) NOT NULL,
+        last_name VARCHAR(100) NOT NULL,
+        username VARCHAR(100) NOT NULL,
+        token VARCHAR(255) NOT NULL,
+        token_expire TIME NOT NULL,
         PRIMARY KEY (id)
-    ) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+    ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
 
     $pdo->exec($sql);
-    echo "Table `login_history` created successfully!";
+
+    $sqlInsert = "INSERT INTO ms_365_instructor (id, first_name, last_name, username, token, token_expire) VALUES
+        ('1', 'Alvine', 'Billones', 'Alvine.Billones@mcclawis.edu.ph', '', '00:00:00'),
+        ('2', 'Juniel', 'Marfa', 'Juniel.Marfa@mcclawis.edu.ph', '', '00:00:00'),
+        ('3', 'Kurt Bryan', 'Alegre', 'KurtBryan.Alegre@mcclawis.edu.ph', '', '00:00:00'),
+        ('4', 'Dino', 'Ilustrisimo', 'Dino.Ilustrisimo@mcclawis.edu.ph', '', '00:00:00'),
+        ('5', 'Jessica', 'Alcazar', 'Jessica.Alcazar@mcclawis.edu.ph', '', '00:00:00'),
+        ('6', 'Jered', 'Cueva', 'Jered.Cueva@mcclawis.edu.ph', '', '00:00:00'),
+        ('7', 'Danilo', 'Villarino', 'Danilo.Villarino@mcclawis.edu.ph', '', '00:00:00'),
+        ('8', 'Jamaica Fe', 'Carabio', 'jamaicafe.carabio@mcclawis.edu.ph', '', '00:00:00'),
+        ('9', 'John Michaelle Piedad', 'Robles', 'johnmichaelle.robles@mcclawis.edu.ph', 'fe7e3ac25fc4711d06d2474ee6948682', '05:31:54'),
+        ('10', 'Emily', 'Ilustrisimo', 'Emily.Ilustrisimo@mcclawis.edu.ph', '', '00:00:00');";
+
+    $pdo->exec($sqlInsert);
+
+    echo "Table `ms_365_instructor` created and data inserted successfully!";
 } catch (PDOException $e) {
     die("Database error: " . $e->getMessage());
 }
