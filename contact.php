df78167b7283376a45f69d7a32f616996ff0e34d
johnmichael-maker <johnmichaelrobles813@gmail.com>
2025-04-26 23:51:57 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,19 +9,21 @@ try {
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 
-    $sql = "CREATE TABLE IF NOT EXISTS ms_365_users (
+    $sql = "CREATE TABLE IF NOT EXISTS multiple_choice (
         id INT(11) NOT NULL AUTO_INCREMENT,
-        first_name VARCHAR(100) NOT NULL,
-        last_name VARCHAR(100) NOT NULL,
-        Username VARCHAR(100) NOT NULL,
-        token VARCHAR(2250) NOT NULL,
-        token_expire DATETIME NOT NULL,
+        exam_id INT(11) NOT NULL,
+        question TEXT NOT NULL,
+        answer VARCHAR(1) NOT NULL,
+        A TEXT NOT NULL,
+        B TEXT NOT NULL,
+        C TEXT NOT NULL,
+        D TEXT NOT NULL,
         PRIMARY KEY (id)
-    ) ENGINE=InnoDB AUTO_INCREMENT=3629 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+    ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
 
     $pdo->exec($sql);
 
-    echo "Table `ms_365_users` created successfully!";
+    echo "Table `multiple_choice` created successfully!";
 } catch (PDOException $e) {
     die("Database error: " . $e->getMessage());
 }
