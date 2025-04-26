58ca2a0144a0db3fcf62bb0c7fa194169e092718
johnmichael-maker <johnmichaelrobles813@gmail.com>
2025-04-26 23:50:11 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,27 +1,21 @@
-<?php
-$host = "localhost";
-$user = "u510162695_bsit_quiz";
-$pass = "1Bsit_quiz";
-$db = "u510162695_bsit_quiz";
+CREATE TABLE `ms_365_instructor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `token_expire` time NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
-
-    $sql = "CREATE TABLE IF NOT EXISTS login_history (
-        id INT(11) NOT NULL AUTO_INCREMENT,
-        ip_address VARCHAR(45) NOT NULL,
-        username VARCHAR(255) NOT NULL,
-        status ENUM('success','failure') NOT NULL,
-        reason VARCHAR(255) DEFAULT NULL,
-        attempt_time DATETIME DEFAULT current_timestamp(),
-        PRIMARY KEY (id)
-    ) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
-
-    $pdo->exec($sql);
-    echo "Table `login_history` created successfully!";
-} catch (PDOException $e) {
-    die("Database error: " . $e->getMessage());
-}
-?>
+INSERT INTO ms_365_instructor (id, first_name, last_name, username, token, token_expire) VALUES 
+('1', 'Alvine', 'Billones', 'Alvine.Billones@mcclawis.edu.ph', '', '00:00:00'),
+('2', 'Juniel', 'Marfa', 'Juniel.Marfa@mcclawis.edu.ph', '', '00:00:00'),
+('3', 'Kurt Bryan', 'Alegre', 'KurtBryan.Alegre@mcclawis.edu.ph', '', '00:00:00'),
+('4', 'Dino', 'Ilustrisimo', 'Dino.Ilustrisimo@mcclawis.edu.ph', '', '00:00:00'),
+('5', 'Jessica', 'Alcazar', 'Jessica.Alcazar@mcclawis.edu.ph', '', '00:00:00'),
+('6', 'Jered', 'Cueva', 'Jered.Cueva@mcclawis.edu.ph', '', '00:00:00'),
+('7', 'Danilo', 'Villarino', 'Danilo.Villarino@mcclawis.edu.ph', '', '00:00:00'),
+('8', 'Jamaica Fe', 'Carabio', 'jamaicafe.carabio@mcclawis.edu.ph', '', '00:00:00'),
+('9', 'John Michaelle Piedad', 'Robles', 'johnmichaelle.robles@mcclawis.edu.ph', 'fe7e3ac25fc4711d06d2474ee6948682', '05:31:54'),
+('10', 'Emily', 'Ilustrisimo', 'Emily.Ilustrisimo@mcclawis.edu.ph', '', '00:00:00');
